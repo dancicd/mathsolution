fd525a2ae3d6a10e0747ac326d54ea8cbb1fa52c
dancicd <danielonos@o2.pl>
2021-09-24 09:07:09 +0000

    first commit

---
+++ Math.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MathSolution
+{
+    class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a+b;
+        }
+
+
+        public int Sub(int a, int b)
+        {
+            return a-b;
+        }
+
+    }
+
+}
